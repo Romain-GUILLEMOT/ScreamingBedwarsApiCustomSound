a0ee87da07de61cca9dc39438a11cab800438bf5
romainguillemot <contact@romain-guillemot.dev>
2025-01-25 15:01:47 +0100

    First commit

---
+++ src/main/java/fr/yotapaki/screamingBedWarsApiCustomSound/ScreamingBedWarsApiCustomSound.java
@@ -0,0 +1,17 @@
+package fr.yotapaki.screamingBedWarsApiCustomSound;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ScreamingBedWarsApiCustomSound extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
